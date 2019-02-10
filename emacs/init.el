dc66a0b3fd94ef5fb3d86e1fa6dcaa82b0f72289
david <>
2019-02-10 17:22:26 +0100

    Added OCaml Config

--- emacs/init.el
+++ emacs/init.el
@@ -138,13 +138,32 @@
 ;	  '(lambda () (liquid-types-mode)))
 
 
-
+;;----------------------------------
+;; OCaml Mode
+;;----------------------------------
+;; Tuareg
+(load "~/ocaml/tuareg/tuareg-site-file")
+(dolist
+   (var (car (read-from-string
+	      (shell-command-to-string "opam config env --sexp"))))
+  ;; Merlin
+  (setenv (car var) (cadr var)))
+(let ((opam-share (ignore-errors (car (process-lines "opam" "config" "var" "share")))))
+  (when (and opam-share (file-directory-p opam-share))                                                                                             
+    ;; Register Merlin
+    (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
+    (autoload 'merlin-mode "merlin" nil t nil)
+    ;; Automatically start it in OCaml buffers
+    (add-hook 'tuareg-mode-hook 'merlin-mode t)
+    (add-hook 'caml-mode-hook 'merlin-mode t)
+    ;; Use opam switch to lookup ocamlmerlin binary
+    (setq merlin-command 'opam)))
 
 ;; -----------------------------------------------------------------------------
 ;; Idris
 ;; -----------------------------------------------------------------------------
 
-(require 'idris-mode)
+;; (require 'idris-mode)
 
 ;; -----------------------------------------------------------------------------
 ;; Lean
